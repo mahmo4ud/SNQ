3ba050f7e3eb9bdc5abf2f62872b3d215a0bfb50
bekohaty <bekohaty@gmail.com>
2025-10-15 21:44:40 +0300

    debuging the Width of hero-tite component UI

--- src/components/hero-title.tsx
+++ src/components/hero-title.tsx
@@ -8,7 +8,7 @@ import mobileLogo from '@/../public/mobile-logo.png'
 export default function HeroTitle({ children }: { children: string }) {
     const { t } = useT("about-us")
     return (
-      <div className="w-screen h-[65vh] flex items-center justify-center relative overflow-hidden">
+      <div className="w-full h-[65vh] flex items-center justify-center relative overflow-hidden">
         <Image src={SubTitleBg} alt="SubTitleBg" className="absolute top-0 left-0 w-full h-full object-cover"/>
         <div className="relative w-full">
           <Image src={mobileLogo} alt="mobileLogo" className="absolute top-6 md:top-30 opacity-10 blur-[2px] left-1/2 -translate-x-1/2 -translate-y-1/2 w-60 md:w-130 object-cover"/>
