9daf1d8b80cbc3a1876819ab255f67b4ee0dcfee
Mahmoud <mahmo4udi@gmail.com>
2025-10-23 21:05:39 +0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export",
-  eslint: { ignoreDuringBuilds: true },
-  images: { unoptimized: true },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [],
+    unoptimized: false,
+  },
 };
 
 export default nextConfig;
