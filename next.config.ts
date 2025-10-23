1a65c486c84f4939f748eb9e1e4ef8690e1640d9
Mahmoud <mahmo4udi@gmail.com>
2025-10-23 20:20:22 +0300

    fix vv

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  images: {
-    remotePatterns: [],
-    unoptimized: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
