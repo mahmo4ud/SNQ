08fd69b447209b8e828fd5b40e93e6a97db617c1
Mahmoud <mahmo4udi@gmail.com>
2025-10-24 10:21:42 +0300

    Update

--- src/app/[lng]/(home)/layout.tsx
+++ src/app/[lng]/(home)/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import { ReactNode } from "react";
 
 export const metadata: Metadata = {
-  title: "الرئيسية",
+  title: "شركة سعيد بن ناصر بن قراد للمحاماة والاستشارات القانونية",
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
